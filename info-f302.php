673f6c4b3a2b6ff2145891bfd622d68a02a15e31
Rodrigue Van Brande <rodriguevb@gmail.com>
2016-06-02 12:21:05 +0200

    [add] info-f302

---
+++ info-f302.php
@@ -0,0 +1,24 @@
+<h1>INFO-F-302 - Informatique Fondamentale</h1>
+<h2>Examen</h2>
+<h3>Théorie</h3>
+
+
+
+<h4 class="question">Donner quatre méthodes vues en cours pour tester si une formule de la logique propositionnelle est valide ou non. Pour chaque méthode, expliquer la en une ligne ou deux.</h4>
+<div class="answer">
+	<ol>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ol>
+</div>
+
+
+
+<h4 class="question">Soient Pet P' deux problèmes de décision. Que signifie l'expression " P se réduit à P' en temps polynomial" ? (indication : 5 lignes max</h4>
+<div class="answer">
+</div>
+
+
+<h3>Pratique</h3>
