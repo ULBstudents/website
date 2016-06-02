28c1f6cc3dd22ffdfc1e51fa6408d2807f8eadac
Rodrigue Van Brande <rodriguevb@gmail.com>
2016-06-02 12:29:53 +0200

    [add] questions

--- info-f302.php
+++ info-f302.php
@@ -4,6 +4,9 @@
 
 
 
+
+
+
 <h4 class="question">Donner quatre méthodes vues en cours pour tester si une formule de la logique propositionnelle est valide ou non. Pour chaque méthode, expliquer la en une ligne ou deux.</h4>
 <div class="answer">
 	<ol>
@@ -16,9 +19,60 @@
 
 
 
+
+
+
+<h4 class="question">Forme normale conjonctive  - Expliquer de manière précise comment on peut mettre une formule sous forme normale conjonctive (donner toutes les règles qu'on devra potentiellement utiliser). Illustrer sur la formule suivante vue en cours (i.e. la mettre sous forme normale conjonctive) :
+$$\neg(p \leftrightarrow (q \rightarrow r))$$</h4>
+<div class="answer">
+</div>
+
+
+
+
+<h4 class="question">Mise sous forme clausale - Donner les différentes étapes de transformations qui permettent de mettre une formule de la logique du premier ordre sous forme clausale (donner un ordre dans lequel les appliquer).
+Expliquer brièvement en quoi consiste chaque étape (une phrase par étape).
+Illustrer par un exemple de votre choix.
+Est-ce que chaque étape de transformation produit nécessairement une formule équivalente à la formule qu'on transforme ?</h4>
+<div class="answer">
+</div>
+
+
+
+
 <h4 class="question">Soient Pet P' deux problèmes de décision. Que signifie l'expression " P se réduit à P' en temps polynomial" ? (indication : 5 lignes max</h4>
 <div class="answer">
 </div>
+
+
+
+
+
+<h4 class="question">Dire des affirmations suivantes si elles sont vraies ou fausses.
+<ol>
+	<li>toute formule de la logique propositionnelle est équivalente à une formule qui n'utilise que
+	les connecteurs ! et :. Justifer votre réponse.</li>
+	<li>il existe des problèmes NP-complets qui sont indécidables. Justifer votre réponse.</li>
+	<li>il existe des problèmes de la classe NP qu'on peut résoudre en temps polynomial. Justifier votre réponse.</li>
+</ol>
+</h4>
+<div class="answer">
+</div>
+
+
+
+
+
+
+<h4 class="question">Question 2 (2 points) Donner les règles à utiliser pour mettre une formule de la logique premier
+ordre sous forme prénexe. Peut-on le faire en temps polynomial (dans le nombre de symboles de
+la formule) ? Justifier.</h4>
+<div class="answer">
+</div>
+
+
+
+
 
 
 <h3>Pratique</h3>
