7b7b7289669a07a8a674ba59fd66e442e983c081
Rodrigue Van Brande <rodriguevb@gmail.com>
2016-07-28 12:32:35 +0200

    [fix] answer

--- info-f310.php
+++ info-f310.php
@@ -12,13 +12,13 @@
 
 
 
-<h4 class="question">Une fabrique de conserve exp´edie des caisses ver des d´epˆots. Nous voulons
-que le programme d’exp´edition des caisses minimise le cot de transport
-total des usines ver les d´epˆots. Pour simplifier, nous supposerons qu’il y a
-deux usine I et II et trois d´epˆots A, B, C. Les disponibilit´e en caisse sont
-respectivement 350 et 550 pour l’usine I et II et le besoin des trois d´epˆots
-est 300. Le coˆut d’expedition, par caisse, entre chaque usine et chaque
-d´epˆot est dans le tableau ci-dessous:
+<h4 class="question">Une fabrique de conserve expédie des caisses ver des dépôts. Nous voulons
+que le programme d’expédition des caisses minimise le cot de transport
+total des usines ver les dépôts. Pour simplifier, nous supposerons qu’il y a
+deux usine I et II et trois dépôts A, B, C. Les disponibilité en caisse sont
+respectivement 350 et 550 pour l’usine I et II et le besoin des trois dépôts
+est 300. Le coût d’expedition, par caisse, entre chaque usine et chaque
+dépôt est dans le tableau ci-dessous:
 
 <ul>
 	<li>Une firme automobile a trois usines à Los Angeles, Detroit et New Orleans, et deux centres de distribution à Denver et Miami.</li>
@@ -91,7 +91,7 @@ On peut représenter le tableau de la manière suivante :
 	</tfoot>
 </table>
 
-On commence à distribuer les offres en faisant attention de ne pas dépasser la demande :
+On commence à distribuer les offres en faisant attention de ne pas dépasser la demande. Si l'offre n'est pas égale à la demande, on obtient un modèle non balancé et on introduit une source ou destination artificielle ; ce n'est pas le cas ici car la demande = offre à 900.
 <table>
 	<thead>
 		<tr>
@@ -106,15 +106,15 @@ On commence à distribuer les offres en faisant attention de ne pas dépasser la
 		<tr>
 			<th>I</th>
 			<td>25<br><b>300</b></td>
-			<td>17<b>50</b></td>
+			<td>17<br><b>50</b></td>
 			<td>16</td>
 			<td>350</td>
 		</tr>
 		<tr>
 			<th>II</th>
 			<td>24</td>
-			<td>18<b>250</b></td>
-			<td>14<b>300</b></td>
+			<td>18<br><b>250</b></td>
+			<td>14<br><b>300</b></td>
 			<td>550</td>
 		</tr>
 		<tr>
@@ -128,6 +128,48 @@ On commence à distribuer les offres en faisant attention de ne pas dépasser la
 	<tfoot>
 	</tfoot>
 </table>
-
+Ce n'est pas la meilleur optimisation. Pour optimiser, on résout récursivement le système (Attention aux erreurs d'inattention).
+$$o_i+d_j-c_{ij}=0 \text{ pour tout } x_{ij} > 0$$
+$$init \Rightarrow o_1 = 0$$
+$$o_1 + d_1 = 25\Rightarrow d_1 = 25$$
+$$o_1 + d_2 = 17\Rightarrow d_2 = 17$$
+$$o_2 + d_2 = 18\Rightarrow o_2 = 1$$
+$$o_2 + d_3 = 14\Rightarrow d_3 = 13$$
+<table>
+	<thead>
+		<tr>
+			<th></th>
+			<th>A<b>25</b></th>
+			<th>B<b>17</b></th>
+			<th>C<b>13</b></th>
+			<th>Offre</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th>I<br><b>0</b></th>
+			<td>25<br><b>300</b></td>
+			<td>17<br><b>50</b></td>
+			<td>16</td>
+			<td>350</td>
+		</tr>
+		<tr>
+			<th>II<br><b>1</b></th>
+			<td>24</td>
+			<td>18<br><b>250</b></td>
+			<td>14<br><b>300</b></td>
+			<td>550</td>
+		</tr>
+		<tr>
+			<th>Demande</th>
+			<td>300</td>
+			<td>300</td>
+			<td>300</td>
+			<td>900</td>
+		</tr>
+	</tbody>
+	<tfoot>
+	</tfoot>
+</table>
 </div>
 
