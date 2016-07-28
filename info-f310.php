7cc48e53f1d4facc9ac6fdce4590e9fead0927ae
Rodrigue Van Brande <rodriguevb@gmail.com>
2016-07-28 12:06:32 +0200

    [fix] answer

--- info-f310.php
+++ info-f310.php
@@ -23,16 +23,73 @@ d´epˆot est dans le tableau ci-dessous:
 <ul>
 	<li>Une firme automobile a trois usines à Los Angeles, Detroit et New Orleans, et deux centres de distribution à Denver et Miami.</li>
 	<li>Les capacités des trois usines sont de 1000, 1500 et 1200 respectivement, et les demandes aux centres de distribution sont de 2300 et 1400 voitures.</li>
-	<li>Coûts :$$\begin{tabular}{c|c|c|c}
-	&\text{A}&\text{B}&\text{C}\\\hline
-	\text{I}&25&17&16\\
-	\text{II}&24&18&14\\
-	\end{tabular}$$</li>
+	<li>Coûts :
+	<table>
+		<thead>
+			<tr>
+				<th></th>
+				<th>A</th>
+				<th>B</th>
+				<th>C</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>I</td>
+				<td>25</td>
+				<td>17</td>
+				<td>16</td>
+			</tr>
+			<tr>
+				<td>II</td>
+				<td>24</td>
+				<td>18</td>
+				<td>14</td>
+			</tr>
+		</tbody>
+		<tfoot>
+		</tfoot>
+	</table></li>
 </li>
 </ul></h4>
 <div class="answer">
 On peut représenter le tableau de la manière suivante :
-$$\begin{tabular}{c|c|c|c||c}&\text{A}&\text{B}&\text{C}&\text{Offre}\\\hline\text{I}&25&17&16&350\\\text{II}&24&18&14&550\\\hline\hlineDemande&300&300&300&900\\\end{tabular}$$
+<table>
+	<thead>
+		<tr>
+			<th></th>
+			<th>A</th>
+			<th>B</th>
+			<th>C</th>
+			<th>Offre</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th>I</th>
+			<td>25</td>
+			<td>17</td>
+			<td>16</td>
+			<td>350</td>
+		</tr>
+		<tr>
+			<th>II</th>
+			<td>24</td>
+			<td>18</td>
+			<td>14</td>
+			<td>550</td>
+		</tr>
+		<tr>
+			<th>Demande</th>
+			<td>300</td>
+			<td>300</td>
+			<td>300</td>
+			<td>900</td>
+		</tr>
+	</tbody>
+	<tfoot>
+	</tfoot>
+</table>
 
 On commence à distribuer les offres en faisant attention de ne pas dépasser la demande :
 $$
