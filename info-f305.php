c4c7fc44a6d73e7850c9df754a39dbc7de4db806
Rodrigue Van Brande <rodriguevb@gmail.com>
2016-08-02 14:08:50 +0200

    [fix] answer

--- info-f305.php
+++ info-f305.php
@@ -56,16 +56,17 @@
 			$$J=\left(\begin{array}{cc}1&2(-1)\\0&3(-1)^2\end{array}\right)=\left(\begin{array}{cc}1&-2\\0&3\end{array}\right)$$
 			Valeurs propres:
 			$$
-				\begin{array}{rcl}
-				det(J-\lambda I) &=&0\\
-				\left|\begin{array}{rr}1-\lambda&-2\\0&3-\lambda\end{array}\right|&=&0\\
-				-(1-\lambda).-(3-\lambda)-(-2).0&=&0\\
-				(-1+\lambda).(-3+\lambda)&=&0\\
-				\lambda^2-4\lambda+3&=&0
-				\end{array}
+			\begin{array}{rcl}
+			0 &=& det(J-\lambda I)\\
+			  &=& \left|\begin{array}{rr}1-\lambda&-2\\0&3-\lambda\end{array}\right|\\
+			  &=& -(1-\lambda).-(3-\lambda)-(-2).0\\
+			  &=& (-1+\lambda).(-3+\lambda)\\
+			  &=& \lambda^2-4\lambda+3
+			\end{array}
 			$$
 			$$\Delta=16-4.1.3=16-12=4$$
-			Solution: $\left(\begin{array}{c}\dfrac{4+\sqrt{4}}{2}\\\dfrac{4-\sqrt{4}}{2}\end{array}\right)=\left(\begin{array}{c}3\\1\end{array}\right)$&larr;Valeurs propres>0, noeud instable!
+			Solution: $\left(\begin{array}{c}\dfrac{4+\sqrt{4}}{2}\\\dfrac{4-\sqrt{4}}{2}\end{array}\right)=\left(\begin{array}{c}3\\1\end{array}\right)$
+			 &larr; Les valeurs propres $ > 0$ &larr; noeud instable!
 		</li>
 		<li>
 			$$\left\{\begin{array}{rcl}\dot{x}_1& = & 0\\\dot{x}_2 & = & 0\end{array}\right.\Leftrightarrow\left\{\begin{array}{rcl}x_2^2+x_1& = & 0\\x_2^3+1 & = & 0\end{array}\right.\Leftrightarrow\left\{\begin{array}{rcl}x_1& = & -x_2^2\\x_2^3 & = & -1\end{array}\right.\Leftrightarrow\left\{\begin{array}{rcl}x_1& = & -1\\x_2 & = & -1\end{array}\right.$$
@@ -143,9 +144,31 @@ sont compatibles avec le système et motiver la réponse.<figure><img src="image
 \left(\begin{array}{cc}\dfrac{(x_1^2+x_2^2-4)'}{\delta x_1}&\dfrac{(x_1^2+x_2^2-4)'}{\delta x_2}\\\dfrac{(x_1^2-x_2-2)'}{\delta x_1}&\dfrac{(x_1^2-x_2-2)'}{\delta x_2}\end{array}\right)=
 \left(\begin{array}{cc}2x_1&2x_2\\2x_1&-1\end{array}\right)$$
 			<ul>
-			<li>La matrice en $\bar{x}^{(1)}$ est $\left(\begin{array}{rr}2\sqrt{3}&2\\2\sqrt{3}&-1\end{array}\right)$. Le point $\bar{x}^{(1)}$ est une selle pour le système linéarisé, donc le point d'équilibre $\bar{x}^{(1)}$ est instable pour le système original.</li>
-			<li>La matrice en $\bar{x}^{(2)}$ est $\left(\begin{array}{rr}-2\sqrt{3}&2\\-2\sqrt{3}&-1\end{array}\right)$. Le point $\bar{x}^{(2)}$ est un foyer asymptomatiquement stable pour le système linéarisé. Donc le point d'équilibre $\bar{x}^{(2)}$ est asymptotiquement stable pour le système original.</li>
-			<li>La matrice en $\bar{x}^{(3)}$ est $\left(\begin{array}{rr}0&-4\\0&-1\end{array}\right)$. Ce système est non simple puisqu une valeur propre est égale à 0. Nous ne pouvons rien déduire sur la stabilité du point $\bar{x}^{(3)}$ dans le système originale.</li>
+			<li>La matrice en $\bar{x}^{(1)}$ est $\left(\begin{array}{rr}2\sqrt{3}&2\\2\sqrt{3}&-1\end{array}\right)$.
+			$$
+			\begin{array}{rcl}
+			0 &=& det(J-\lambda I)\\
+			&=& \left|\begin{array}{rr}2\sqrt{3}-\lambda&2\\2\sqrt{3}&-1-\lambda\end{array}\right|\\
+			&=& -(2\sqrt{3}-\lambda).-(-1-\lambda)-2.2\sqrt{3}\\
+			&=& (2\sqrt{3}+\lambda).(1+\lambda)-4\sqrt{3}\\
+			&=& 2\sqrt{3}+\lambda+2\sqrt{3}\lambda+\lambda^2-4\sqrt{3}\\
+			&=& \lambda^2+(1+2\sqrt{3})\lambda-2\sqrt{3}
+			\end{array}
+			$$
+			Le point $\bar{x}^{(1)}$ est une selle pour le système linéarisé, donc le point d'équilibre $\bar{x}^{(1)}$ est instable pour le système original.</li>
+			<li>La matrice en $\bar{x}^{(2)}$ est $\left(\begin{array}{rr}-2\sqrt{3}&2\\-2\sqrt{3}&-1\end{array}\right)$.
+			Le point $\bar{x}^{(2)}$ est un foyer asymptomatiquement stable pour le système linéarisé. Donc le point d'équilibre $\bar{x}^{(2)}$ est asymptotiquement stable pour le système original.</li>
+			<li>La matrice en $\bar{x}^{(3)}$ est $\left(\begin{array}{rr}0&-4\\0&-1\end{array}\right)$.
+			$$
+			\begin{array}{rcl}
+			0 &=& det(J-\lambda I)\\
+			&=& \left|\begin{array}{rr}0-\lambda&-4\\0&-1-\lambda\end{array}\right|\\
+			&=& -(-\lambda).-(-1-\lambda)-(-4).0\\
+			&=& (\lambda).(1+\lambda)\\
+			&=& \lambda^2+\lambda
+			\end{array}
+			$$
+			Ce système est non simple puisqu une valeur propre est égale à 0. Nous ne pouvons rien déduire sur la stabilité du point $\bar{x}^{(3)}$ dans le système originale.</li>
 			</ul>
 		</li>
 		<li>
